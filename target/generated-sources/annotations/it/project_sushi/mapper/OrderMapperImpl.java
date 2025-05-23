c5bb18559a55ee7ae91a55df1025b4febed2f4d0
MKAndrea <andreachristian.bonafede@gmail.com>
2025-05-23 17:11:44 +0200

    Aggiunto BackEnd

--- target/generated-sources/annotations/it/project_sushi/mapper/OrderMapperImpl.java
+++ target/generated-sources/annotations/it/project_sushi/mapper/OrderMapperImpl.java
@@ -12,8 +12,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-04-16T17:17:48+0200",
-    comments = "version: 1.6.0.Beta1, compiler: Eclipse JDT (IDE) 3.36.0.v20231114-0937, environment: Java 17.0.9 (Eclipse Adoptium)"
+    date = "2025-05-23T17:07:32+0200",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.36.0.v20231114-0937, environment: Java 17.0.9 (Eclipse Adoptium)"
 )
 @Component
 public class OrderMapperImpl implements OrderMapper {
